c70c496ba4297bf240a0cbf4fc6f459ed417929a
Lofstead <gflofst@sandia.gov>
2021-01-19 13:47:31 -0700

    version 1.1 release

--- README.rst
+++ README.rst
@@ -1,57 +1,196 @@
-Additive-IO
-===========
+Stitch IO
+=========
 
-by Jay Lofstead, John Mitchell, and Steve Plimpton
+by Jay Lofstead (gflofst@sandia.gov) and John Mitchell (jamitch@sandia.gov)
+with consulting by Steve Plimpton
 
-This project aims to develop an new, efficient IO library optimized for the
-style of operation found in the SPPARKS kinetic monte carlo application
-(http://spparks.sandia.gov)
+This project aims to develop a new, efficient IO library optimized for the
+style of operation found in the SPPARKS kinetic monte carlo applications
+(http://spparks.sandia.gov).  This novel library was motivated by additive 
+manufacturing applications where material is incrementally added to a part; 
+using the 'Stitch' libary, data/material is appended to a file much in the same 
+way material is incrementally added to a part; in this way, simulations of the 
+additive manufacturing process are conducted locally, ie only on that portion 
+of the build which is active.  This approach to IO is very efficient computationally 
+since only the active portion of build is computed on; furthermore, the output 
+database is extremely compact since state is only written to the file when 
+it changes.
 
-The initial design point is using a key-value store for each active region
-written with a metadata engine to track all of the basic information and a
-generative naming system for finding data later.
 
-The tests and Python interface all assume a current Python 3.x implementation.
+Build 
+-----
 
-The current build setup is fragile, but works. Here is how to make it work:
+There are two different entities which may be built.
 
-1. Go to the libstitch directory
-2. 'make stitch_module' will create a serial version of the library for Python use and install it in the specified installation directory.
-3. 'make stitch.lib' builds a parallel C interface suitable for linking with anything.
-4. 'make stitch_test' builds the parallel C test harness that tests the API, but not the data.
+#. libstitch.a; for use with c/c++ applications; can be built for serial
+   or parallel calculations.
 
-To test the library correctness (serial interface):
+#. python module; must have python 3, numpy, scipy.  
 
-1. build the Python version (serial)
-2. go into integrated_test/weld/potts
-3. make clean ; make
+Below, STITCH denotes the directory where 'Stitch' is cloned.
 
-This should run clean.
+Build the 'stitch' python module
+--------------------------------
+
+The 'stitch' python module allows for creating, reading and writing 'stitch'
+files.  It is particularly useful for reading 'stitch' files written by
+'spparks'.
+
+The python module should be built with gnu gcc.  It may be possible to do
+otherwise but that has not been tested.  In general, the same compiler used to
+build python, numpy, and scipy should be used to build the stitch module.
+
+Python 3, numpy, and scipy are required for building the 'stitch' python
+module.  This documentation was developed with the following versions.
+
+* gnu gcc 9.2.0
+* Python 3.6.9
+* numpy '1.19.2'
+* scipy '1.5.2'
+
+Other versions for all of the above will probably work provided 
+python 3 is used.
+
+The python module is generally built for serial applications that post process
+stitch files; this is the default and is recommended.  gcc or other compatible
+compilers are used to build the python module.  The python module may be built
+with an mpi compiler but will remain a serial python interface to a stitch
+file.  
+
+Change to stitch directory 
+
+::
+
+   % cd $STITCH
+
+In the root project directory, create a setup.cfg file appropriate for your
+system. Look at the other examples for guidance. This is important for the
+python build setup.  The file 'setup.flamer.cfg' and others are included in
+this directory; copy one of these to setup.cfg and edit appropriately; specify
+where the python module should be installed -- then add this path to your
+PYTHONPATH.
+
+Build and install the python module.
+
+::
+
+    % python setup.py build
+    % python setup.py install
+
+Some build 'warnings' may occur which are generally harmless.  On the other
+hand 'errors' are not good and must be addressed.  
+
+Verify install and PYTHONPATH are correctly set by launching python and
+importing the stitch module. Make sure not to do this in the source directory.
+
+If the following command succeeds, python module build and install is
+probably good.  Start an interactive python session by launching python 
+and import the stitch module:
+
+::
+
+    % python
+    % from stitch.libstitch import libstitch
+
+As final verification of python module correctness, run the unit test; remove
+all stitch files prior to running the test -- otherwise the test will fail.
+
+:: 
+
+    % cd $STITCH/integrated_test/weld/potts/
+    % rm *.st
+
+
+Run unit test
+
+::
+
+    % python unit_cv_readwrite.py
+
+
+Screen output from test should look something like:
+
+::
+
+   .........
+   ----------------------------------------------------------------------
+   Ran 9 tests in 9.383s
+   
+   OK
+
+
+The python module may be built for parallel python applications 
+using mpi4py -- this works but is not as well
+exercised and tested and is not recommended as its still an area of
+development.  To build a parallel Python module:
+
+* Build and/or install mpi4py; make sure import mpi4py works
+* Add -DSTITCH_PARALLEL to the extra_compiler_args list in libstitch/setup.py
+* build and test python module as described for the serial python module build
+* See Makefile on howto run parallel python tests
+* Should pass all tests.
+
+
+Build the 'stitch' library for c/c++ application development
+------------------------------------------------------------
+
+::
+
+    % cd $STITCH/libstitch
+
+
+It is possible to build a serial C library or a Parallel Python library.  
+
+Edit path to c compiler by appropriately defining CC in the 'Makefile'.  Use
+the same mpi compiler to build the stitch library as will be used for the
+parallel application.  Use -DSTITCH_PARALLEL macro for parallel applications
+such as the SPPARKS Kinetic Monte Carlo simulation framework.  To build a
+serial C library edit the Makefile and remove -DSTITCH_PARALLEL from both
+stitch_test and stitch.lib.
+
+Build libstitch.a suitable for linking with c/c++ applications; 
+
+::
+
+    % make stitch.lib  
+
+Build 'stitch_test' written in C; this test exercises the c/c++ parallel API;
+
+::
+
+    % make stitch_test 
 
 To test the C API correctness (parallel):
 
-1. build the stitch_test test harness
-2. mpiexec -np 2 stitch_test 2 1 1
+::
 
-This should just output a bunch of 3-d box corner pairs without any other output. If this happens, it is working correctly.
+    % mpiexec -np 4 stitch_test
 
-It is possible to build a serial C library or a Parallel Python library.
+Depending on the mode selected, it will either test (default), generate output
+to the console, or write to files to configure a new testing dataset. For the
+console output, it should just output a bunch of 3-d box corner pairs and the
+values for proc 0. If this happens and it looks right, it is probably working
+correctly. The code is configured to be adaptable, but is currently assuming a
+2x2x1 process layout per timestep with each process having a 2x2x3 chunk of the
+domain. It increments in x and y by 3 and z by 1. Since there are no
+incorporated automated testing, it can be scaled and visually tested by
+adapting the internal parameters. There are four sets of relevant parameters:
+the global (total) domain size, the process count in each dimension, how big
+the decomposition is in each dimension, and how far to move when incrementing
+in that dimension.
 
-To build a serial C library:
 
-1. edit the Makefile to remove the -DSTITCH_PARALLEL from both stitch_test and stitch.lib.
-2. make stitch.lib ; make stitch_test
-3. stitch_test
+TROUBLESHOOTING
+---------------
 
-To build a parallel Python library:
+* Stitch's file format (SQLite3) relies on the file system to properly handle
+  POSIX locks to manage concurrency. GPFS mostly to completely does this
+  properly. Lustre does not do this at all. To work around this issue, the
+  parallel version uses token passing to serialize access. This is created with
+  -DSTITCH_PARALLEL. If there are issues related to this, please contact us for
+  an additional workaround.
 
-1. Figure out your mpi4py import statement.
-2. edit setup.py to change the mpi4py import to be what is correct for you.
-3. Add -DSTITCH_PARALLEL to the extra_compiler_args list.
-4. Make stitch_module
-5. go to integrated_tests/weld/potts
-6. edit the Makefile
-7. comment the serial test case out
-8. uncomment the two parallel test cases
-9. make
-10. This should pass all tests.
+* The library is written to handle concurrency by retrying. It may fail, but it
+  should not. There are a lot of error messages that include line numbers,
+  function names and other information. Please include that info with any bug
+  report.
--- libstitch/stitch_test.c
+++ libstitch/stitch_test.c
@@ -25,10 +25,14 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include "stitch.h"
 
-void print_block (const char * prefix, int32_t * bb, int32_t * state)
+// to generate testing data, set this to 1. Otherwise output goes to stdout
+#define STITCH_TESTING_DATA 0
+
+void print_block (FILE * f, const char * prefix, int32_t * bb, int32_t * state)
 {
     int32_t x1 = bb [0];
     int32_t x2 = bb [1];
@@ -37,21 +41,65 @@ void print_block (const char * prefix, int32_t * bb, int32_t * state)
     int32_t z1 = bb [4];
     int32_t z2 = bb [5];
     int32_t offset = 0;
-    printf ("%s ", prefix);
-    printf ("(%d, %d, %d) - (%d, %d, %d)\n", x1, y1, z1, x2, y2, z2); 
+#if STITCH_TESTING_DATA
+    // when generating new test data, use this version of the function to get
+    // the values after a proper visual inspection
     for (int32_t z = z1; z < z2; z++)
     {
-        printf ("z(%d) ", z);
         for (int32_t y = y1; y < y2; y++)
         {
-            printf ("y(%d) ", y);
             for (int32_t x = x1; x < x2; x++)
             {
-                printf ("%d ", state [offset++]);
+                fprintf (f, "%2d,", state [offset++]);
             }
-            printf ("\n");
         }
     }
+    fprintf (f,"\n");
+#else
+    fprintf (f, "%s ", prefix);
+    fprintf (f, "(%d, %d, %d) - (%d, %d, %d)\n", x1, y1, z1, x2, y2, z2); 
+    for (int32_t z = z1; z < z2; z++)
+    {
+        fprintf (f, "z(%d)\n", z);
+        for (int32_t y = y1; y < y2; y++)
+        {
+            fprintf (f, "  y(%d) ", y);
+            for (int32_t x = x1; x < x2; x++)
+            {
+                fprintf (f, "%3d ", state [offset++]);
+            }
+            fprintf (f, "\n");
+        }
+    }
+#endif
+}
+
+// compare the read block against the key
+int test_stitch_block (int rank, int32_t * bb, int32_t * state, int32_t * test_data)
+{
+    int32_t x1 = bb [0];
+    int32_t x2 = bb [1];
+    int32_t y1 = bb [2];
+    int32_t y2 = bb [3];
+    int32_t z1 = bb [4];
+    int32_t z2 = bb [5];
+    int32_t offset = 0;
+    for (int32_t z = z1; z < z2; z++)
+    {
+        for (int32_t y = y1; y < y2; y++)
+        {
+            for (int32_t x = x1; x < x2; x++)
+            {
+                if (state [offset] != test_data [offset])
+                {
+                    fprintf (stderr, "test failed. Rank: %d, (%d. %d, %d) %d does not match %d\n", rank, x, y, z, state [offset], test_data [offset]);
+                    exit (-1);
+                }
+                offset++;
+            }
+        }
+    }
+    return 1;
 }
 
 int main (int argc, char ** argv)
@@ -61,16 +109,24 @@ int main (int argc, char ** argv)
     double time = 1.0; // goes up by 1 each time
     double time_increment = 1.0;
 
-    int increment = 2; // how much to move for each block
+    int x_increment = 3; // how much to move for each block when we move in that direction
+    int y_increment = 3; // how much to move for each block when we move in that direction
+    int z_increment = 1; // how much to move for each block when we move in that direction
 
-    int32_t g_x1 = 1; // global size
-    int32_t g_y1 = 1;
-    int32_t g_z1 = 1;
-    int32_t g_x2 = 8; // global size
-    int32_t g_y2 = 8;
-    int32_t g_z2 = 8;
+    int np_x = 2; // number of procs in x
+    int np_y = 2; // number of procs in y
+    int np_z = 1; // number of procs in z
 
-    int block_size = 4; // how big to get in each dimension
+    int32_t g_x1 = 0; // global size
+    int32_t g_y1 = 0;
+    int32_t g_z1 = 0;
+    int32_t g_x2 = 13; // global size
+    int32_t g_y2 = 13;
+    int32_t g_z2 = 4; // do a single layer
+
+    int x_block_size = 2; // how big to get in each dimension
+    int y_block_size = 2; // how big to get in each dimension
+    int z_block_size = 3; // how big to get in each dimension
 
     int32_t x1 = 0;
     int32_t y1 = 0;
@@ -79,16 +135,52 @@ int main (int argc, char ** argv)
     int32_t y2 = 0;
     int32_t z2 = 0;
 
+#include "stitch_test_data.h"
+
     const char * field = "spin";
     int64_t field_id = 0;
 
-    // 3-D = 3 x block_size
-    int32_t * state = (int32_t *) malloc (sizeof (int32_t) * block_size * block_size * block_size);
+    int32_t * state = (int32_t *) malloc (sizeof (int32_t) * x_block_size * y_block_size * z_block_size);
+    int32_t * full_state = malloc ((g_x2 - g_x1) * (g_y2 - g_y1) * (g_z2 - g_z1) * sizeof (int32_t));
+
+    int rank;
+    int size;
 
     MPI_Init (&argc, &argv);
 
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+    assert (size == 4);
+
+    if (rank == 0)
+    {
+        FILE * f = NULL;
+
+#if STITCH_TESTING_DATA
+        f = fopen ("final", "a");
+#else
+        f = stdout;
+#endif
+        fprintf (f, "nprocs in x, y, z: %d, %d, %d\n", np_x, np_y, np_z);
+        fprintf (f, "block size in x, y, z: %d, %d, %d\n", x_block_size, y_block_size, z_block_size);
+        fprintf (f, "increment in x, y, z: %d, %d, %d\n", x_increment, y_increment, z_increment);
+        fprintf (f, "total domain bounds (min) - (max): (%d, %d, %d) - (%d, %d, %d)\n", g_x1, g_y1, g_z1, g_x2, g_y2, g_z2);
+        fprintf (f, "sweeping 0->n in X, then Y, then Z\n");
+        unlink ("test.st");
+
+#if STITCH_TESTING_DATA
+        fclose (f);
+#endif
+    }
+
+    MPI_Barrier (MPI_COMM_WORLD);
+
+    // these are used to step through the testing data
+    int xy_step = 0;
+    int z_step = 0;
+
     stitch_open (&file, MPI_COMM_WORLD, "test.st");
-//    stitch_open (&file, "test.st");
 
     stitch_set_parameters (file, 1.0e-9, 1.0e-15, -1);
     union StitchTypesUnion no_value;
@@ -101,66 +193,110 @@ int main (int argc, char ** argv)
     stitch_query_field (file, field, &field_id);
     assert (field_id != STITCH_FIELD_NOT_FOUND_ID);
 
-    for (int32_t z = g_z1; z < g_z2; z += increment)
+    int32_t bb [6];
+    int32_t new_time = 0;
+    double last_time;
+    for (int32_t z = g_z1; z + z_increment < g_z2; z += z_increment, z_step++)
     {
-        for (int32_t y = g_y1; y < g_y2; y += increment)
+        for (int32_t y = g_y1; y + y_increment < g_y2; y += y_increment)
         {
-            for (int32_t x = g_x1; x < g_x2; x += increment)
+            for (int32_t x = g_x1; x + x_increment < g_x2; x += x_increment, xy_step++)
             {
                 double absolute_tolerance;
                 double relative_tolerance;
                 int64_t no_value_present;
                 double first_time;
-                double last_time;
-                int32_t bb [6];
-                int32_t new_time = 0;
+                new_time = 0;
 
                 stitch_get_parameters (file, &absolute_tolerance, &relative_tolerance, &no_value_present, &first_time, &last_time);
-//                printf ("last_time: %-6.1lf\n", last_time);
 
-                bb [0] = x;
-                bb [1] = x + block_size;
-                bb [2] = y;
-                bb [3] = y + block_size;
-                bb [4] = z;
-                bb [5] = z + block_size;
-                if (bb [1] > g_x2)
-                    bb [1] = g_x2;
-                if (bb [3] > g_y2)
-                    bb [3] = g_y2;
-                if (bb [5] > g_z2)
-                    bb [5] = g_z2;
-printf ("read: (%d, %d, %d) - (%d, %d, %d)\n", bb [0], bb [2], bb [4], bb [1], bb [3], bb [5]);
+                bb [0] = x + (rank % np_x) * np_x;
+                bb [1] = bb [0] + x_block_size;
+                bb [2] = y + ((rank / np_x) % np_y) * np_y;
+                bb [3] = bb [2] + y_block_size;
+                bb [4] = z + (rank / (np_x * np_y)) * np_z;
+                bb [5] = bb [4] + z_block_size;
                 stitch_read_block_int32 (file, field_id, &time, bb, state, &new_time);
 
-//                print_block ("read", bb, state);
+#if 1
+                //if (rank == 0)
+                {
+                    char rank_str [20]; 
+#if STITCH_TESTING_DATA
+                    sprintf (rank_str, "rank_%d", rank);
+                    FILE * f = fopen (rank_str, "a");
 
-                // check the state for -1 values saying that it needs init
+                    print_block (f, rank_str, bb, state);
+
+                    fclose (f);
+#else
+                    sprintf (rank_str, "rank_%d ", rank);
+                    int32_t * test_data = NULL;
+                    //print_block (stdout, rank_str, bb, state);
+                    switch (rank)
+                    {
+                        case 0: test_data = test_rank_0 [xy_step]; break;
+                        case 1: test_data = test_rank_1 [xy_step]; break;
+                        case 2: test_data = test_rank_2 [xy_step]; break;
+                        case 3: test_data = test_rank_3 [xy_step]; break;
+                    }
+                    test_stitch_block (rank, bb, state, test_data);
+#endif
+                }
+#endif
+
 
                 // do compute
                 int32_t offset = 0;
                 int32_t new_val = (int32_t) time;
-//printf ("new_val: %d\n", new_val);
-                for (int32_t n_z = z; n_z < z + block_size; n_z++)
+                for (int32_t n_z = z; n_z < z + z_block_size; n_z++)
                 {
-                    for (int32_t n_y = y; n_y < y + block_size; n_y++)
+                    for (int32_t n_y = y; n_y < y + y_block_size; n_y++)
                     {
-                        for (int32_t n_x = x; n_x < x + block_size; n_x++)
+                        for (int32_t n_x = x; n_x < x + x_block_size; n_x++)
                         {
-//printf ("offset: %d\n", offset);
                             state [offset++] = new_val;
                         }
                     }
                 }
-//printf ("offset: %d\n", offset);
 
                // write new block
+//printf ("[%d] write: (%d, %d, %d) - (%d, %d, %d)\n", rank, bb [0], bb [2], bb [4], bb [1], bb [3], bb [5]);
                stitch_write_block_int32 (file, field_id, &time, bb, state, &new_time);
                time += time_increment;
             }
         }
+
+        bb [0] = g_x1;
+        bb [1] = g_x2;
+        bb [2] = g_y1;
+        bb [3] = g_y2;
+        bb [4] = g_z1;
+        bb [5] = g_z2;
+        double read_time = last_time + 1;
+
+        // parallel call has to be done be all
+        stitch_read_block_int32 (file, field_id, &read_time, bb, full_state, &new_time);
+
+        if (rank == 0)
+        {
+#if STITCH_TESTING_DATA
+            FILE * f = fopen ("final", "a");
+            print_block (f, "final", bb, full_state);
+            fclose (f);
+#else
+            //print_block (stdout, "final", bb, full_state);
+            test_stitch_block (rank, bb, full_state, test_final [z_step]);
+#endif
+        }
     }
 
+    MPI_Barrier (MPI_COMM_WORLD);
+
+    if (rank == 0)
+        printf ("tests passed\n");
+
+cleanup:
     stitch_close (&file);
 
     MPI_Barrier (MPI_COMM_WORLD);
